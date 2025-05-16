d6daebdb3fd679e70fc1abc2e8e29dec944302da
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-05-16 18:10:36 +0530

    update url of restaurant

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
     path('menu/', views.MenuItemView.as_view(), name='menu-list'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
 ]
