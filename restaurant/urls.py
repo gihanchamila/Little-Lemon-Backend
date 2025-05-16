d611602e889b897dc447270a644e18b2fed88458
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-05-16 18:08:11 +0530

    add menu urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index')
+    path('menu/', views.MenuItemView.as_view(), name='menu-list'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
 ]
