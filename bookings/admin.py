0c5bfe067553a2f49ac7232c4e869ee789963bd2
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-13 21:43:24 +0530

    register models with admin sites

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser, Occasion, SeatingType, Booking, Payment, TimeSlot
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Occasion)
+admin.site.register(SeatingType)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(TimeSlot)
