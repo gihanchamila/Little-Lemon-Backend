a8e1cb54bfa15e0fbec6e7409b6ac8b316c708f2
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-14 09:24:38 +0530

    update serializer

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -3,20 +3,24 @@ from .models import Occasion, SeatingType, TimeSlot, CustomUser, Booking, Paymen
 from django.contrib.auth.password_validation import validate_password
 
 class UserCreationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True)
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    re_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password', 'first_name', 'last_name')
+        fields = ('email', 'first_name', 'last_name', 'password', 're_password')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['re_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
 
     def create(self, validated_data):
+        validated_data.pop('re_password')
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            password=validated_data['password'],
+            password=validated_data['password']
         )
         return user
